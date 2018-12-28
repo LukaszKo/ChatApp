047c8f25420a72930e301d35da372ca90aa78525
Lukasz <lukaszkochajews@gmail.com>
2018-12-28 12:31:52 +0100

    removed prod config for db

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,5 @@ module.exports = {
   dev: {
     datebase: 'mongodb://localhost:27017/chat',
     secret: 'yoursecret'
-  },
-  prod: {
-    datebase: 'mongodb://LukaszKo:Szybko10@mongocluster-shard-00-00-pabcs.mongodb.net:27017,mongocluster-shard-00-01-pabcs.mongodb.net:27017,mongocluster-shard-00-02-pabcs.mongodb.net:27017/test?ssl=true&replicaSet=MongoCluster-shard-0&authSource=admin',
-    secret: 'YourSecretPasswordOnProduction'
   }
 }
